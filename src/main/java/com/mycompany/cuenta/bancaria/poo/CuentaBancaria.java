ffdbb01bf3bc09d34fd6474d94ca2d68bb7638ee
Paula Corredor <paulaandrea28112001@gmail.com>
2024-09-05 19:05:39 -0500

    Taller 1 poo

--- src/main/java/com/mycompany/cuenta/bancaria/poo/CuentaBancaria.java
+++ src/main/java/com/mycompany/cuenta/bancaria/poo/CuentaBancaria.java
@@ -1,5 +1,44 @@
 package com.mycompany.cuenta.bancaria.poo;
 
 public class CuentaBancaria {
-    // TODO: Aquí va el código de la clase CuentaBancaria
+    private String titular;
+    private String numeroCuenta;
+    private double saldo;
+    private double tipoInteres=1.5;
+    
+     public CuentaBancaria(String titular, String numeroCuenta, double saldo) {
+        this.titular = titular;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+   public CuentaBancaria(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    // Método para ingresar una cantidad
+    public void ingresar(double cantidad) {
+        if (cantidad > 0) {
+            saldo += cantidad;
+            System.out.println("Has ingresado: " + cantidad + ". Nuevo saldo: " + saldo);
+        } else {
+            System.out.println("La cantidad a ingresar debe ser mayor que cero.");
+        }
+}
 }
--- src/test/java/CuentaBancariaTest.java
+++ src/test/java/CuentaBancariaTest.java
@@ -9,4 +9,16 @@ public class CuentaBancariaTest {
     //      double valorActual = Calculadora.calcular(7, 3, '+');
     //      assertEquals(valorEsperado, valorActual);
     // }
+     public void testSetTipoInteresValido() {
+        // Prueba con un tipo de interés válido (5%)
+        CuentaBancaria.setTipoInteres(5.0);
+        assertEquals(5.0, CuentaBancaria.getTipoInteres(), "El tipo de interés debería ser 5.0%");
+    }
+
+ 
+    public void testSetTipoInteresInvalidoNegativo() {
+        // Prueba con un tipo de interés negativo (-2%)
+        CuentaBancaria.setTipoInteres(-2.0);
+        assertEquals(1.5, CuentaBancaria.getTipoInteres(), "El tipo de interés no debería cambiar de 1.5% al intentar establecer un valor negativo.");
+    }
 }
